e3e522c9e420e79e8598b5aff53bfd757e537640
ruzia <ruzia@idealab.jp>
2013-04-24 12:37:26 +0900

    significant eventを追加し、ダイアログの表示タイミングを変更した

--- src/com/sbstrm/appirater/Appirater.java
+++ src/com/sbstrm/appirater/Appirater.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,96 +33,55 @@ import android.widget.TextView;
 
 public class Appirater {
 
-	private static final String PREF_LAUNCH_COUNT = "launch_count";
-	private static final String PREF_RATE_CLICKED = "rateclicked";
-	private static final String PREF_DONT_SHOW = "dontshow";
-	private static final String PREF_DATE_REMINDER_PRESSED = "date_reminder_pressed";
-	private static final String PREF_DATE_FIRST_LAUNCHED = "date_firstlaunch";
-	private static final String PREF_APP_VERSION_CODE = "versioncode";
-	
+    private static final String PREF_SIGNIFICANT_EVENT_COUNT = "significant_event";
+    private static final String PREF_RATE_CLICKED = "rateclicked";
+    private static final String PREF_DONT_SHOW = "dontshow";
+    private static final String PREF_DATE_REMINDER_PRESSED = "date_reminder_pressed";
+    private static final String PREF_DATE_FIRST_LAUNCHED = "date_firstlaunch";
+
+    private static SharedPreferences getSharedPreferences(Context mContext) {
+        return mContext.getSharedPreferences(mContext.getPackageName()+".appirater", 0);
+    }
+
     public static void appLaunched(Context mContext) {
-    	boolean testMode = mContext.getResources().getBoolean(R.bool.appirator_test_mode);
-        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName()+".appirater", 0);
-        if(!testMode && (prefs.getBoolean(PREF_DONT_SHOW, false) || prefs.getBoolean(PREF_RATE_CLICKED, false))) {return;}
-        
+        SharedPreferences prefs = getSharedPreferences(mContext);
         SharedPreferences.Editor editor = prefs.edit();
-        
-        if(testMode){
-        	showRateDialog(mContext, editor);
-        	return;
-        }
-        
-        // Increment launch counter
-        long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0);
 
         // Get date of first launch
         long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCHED, 0);
-        
-		// Get reminder date pressed
-		long date_reminder_pressed = prefs.getLong(PREF_DATE_REMINDER_PRESSED, 0);
-        
-        try{
-	        int appVersionCode = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionCode;
-	        if(prefs.getInt(PREF_APP_VERSION_CODE, 0)  != appVersionCode){
-	        	//Reset the launch count to help assure users are rating based on the latest version. 
-	        	launch_count = 0;
-	        }
-	        editor.putInt(PREF_APP_VERSION_CODE, appVersionCode);
-        }catch(Exception e){
-        	//do nothing
-        }
-        
-        launch_count++;
-        editor.putLong(PREF_LAUNCH_COUNT, launch_count);
-
         if (date_firstLaunch == 0) {
             date_firstLaunch = System.currentTimeMillis();
             editor.putLong(PREF_DATE_FIRST_LAUNCHED, date_firstLaunch);
         }
-        
-        // Wait at least n days before opening
-        if (launch_count >= mContext.getResources().getInteger(R.integer.appirator_launches_until_prompt)) {
-			long millisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_until_prompt) * 24 * 60 * 60 * 1000L;			
-			if (System.currentTimeMillis() >= (date_firstLaunch + millisecondsToWait)) {
-				if(date_reminder_pressed == 0){
-					showRateDialog(mContext, editor);
-				}else{
-					long remindMillisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_before_reminding) * 24 * 60 * 60 * 1000L;
-					if(System.currentTimeMillis() >= (remindMillisecondsToWait + date_reminder_pressed)){
-						showRateDialog(mContext, editor);
-					}
-				}
-			}
-        }
-        
+
         editor.commit();
-    }   
-    
-	@SuppressLint("NewApi")
-	private static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
-    	String appName = mContext.getString(R.string.appirator_app_title);
+    }
+
+    @SuppressLint("NewApi")
+    private static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
+        String appName = mContext.getString(R.string.appirator_app_title);
         final Dialog dialog = new Dialog(mContext);
-        
+
         if (android.os.Build.VERSION.RELEASE.startsWith("1.") || android.os.Build.VERSION.RELEASE.startsWith("2.0") || android.os.Build.VERSION.RELEASE.startsWith("2.1")){
-        	//No dialog title on pre-froyo devices
-        	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            //No dialog title on pre-froyo devices
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         }else if(mContext.getResources().getDisplayMetrics().densityDpi == DisplayMetrics.DENSITY_LOW || mContext.getResources().getDisplayMetrics().densityDpi == DisplayMetrics.DENSITY_MEDIUM){
-        	Display display = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        	int rotation = display.getRotation();
-        	if(rotation == 90 || rotation == 270){
-        		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        	}else{
-        		dialog.setTitle(String.format(mContext.getString(R.string.rate_title), appName));
-        	}
+            Display display = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+            int rotation = display.getRotation();
+            if(rotation == 90 || rotation == 270){
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            }else{
+                dialog.setTitle(String.format(mContext.getString(R.string.rate_title), appName));
+            }
         }else{
-        	dialog.setTitle(String.format(mContext.getString(R.string.rate_title), appName));
+            dialog.setTitle(String.format(mContext.getString(R.string.rate_title), appName));
         }
 
         LinearLayout layout = (LinearLayout)LayoutInflater.from(mContext).inflate(R.layout.appirater, null);
-        
+
         TextView tv = (TextView) layout.findViewById(R.id.message);
         tv.setText(String.format(mContext.getString(R.string.rate_message), appName));
-        
+
         Button rateButton = (Button) layout.findViewById(R.id.rate);
         rateButton.setText(String.format(mContext.getString(R.string.rate), appName));
         rateButton.setOnClickListener(new OnClickListener() {
@@ -159,7 +119,57 @@ public class Appirater {
             }
         });
 
-        dialog.setContentView(layout);        
-        dialog.show();        
+        dialog.setContentView(layout);
+        dialog.show();
+    }
+
+    public static void userDidSignificantEvent(Context mContext) {
+        SharedPreferences prefs = getSharedPreferences(mContext);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Increment significant event counter
+        long significantEventCount = prefs.getLong(PREF_SIGNIFICANT_EVENT_COUNT, 0);
+
+        significantEventCount++;
+        editor.putLong(PREF_SIGNIFICANT_EVENT_COUNT, significantEventCount);
+        editor.commit();
+    }
+
+    public static void showRateDialogIfNeeded(Context mContext) {
+        boolean testMode = mContext.getResources().getBoolean(R.bool.appirator_test_mode);
+        SharedPreferences prefs = getSharedPreferences(mContext);
+        if(!testMode && (prefs.getBoolean(PREF_DONT_SHOW, false) || prefs.getBoolean(PREF_RATE_CLICKED, false))) {return;}
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        if(testMode){
+            showRateDialog(mContext, editor);
+            return;
+        }
+
+        // Get date of first launch
+        long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCHED, 0);
+
+        // Get reminder date pressed
+        long date_reminder_pressed = prefs.getLong(PREF_DATE_REMINDER_PRESSED, 0);
+
+        // Wait at least n days before opening
+        long millisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_until_prompt) * 24 * 60 * 60 * 1000L;
+        if (System.currentTimeMillis() >= (date_firstLaunch + millisecondsToWait)) {
+            if (date_reminder_pressed != 0) {
+                long remindMillisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_before_reminding) * 24 * 60 * 60 * 1000L;
+                if (System.currentTimeMillis() >= (remindMillisecondsToWait + date_reminder_pressed)) {
+                    Log.d("Appirater", "currentTimeMillis=" + System.currentTimeMillis() + ", remindMillissecondsToWait=" + remindMillisecondsToWait + ", date_reminder_pressed=" + date_reminder_pressed);
+                    showRateDialog(mContext, editor);
+                    return;
+                }
+            } else {
+                long significantEventCount = prefs.getLong(PREF_SIGNIFICANT_EVENT_COUNT, 0);
+                if (mContext.getResources().getInteger(R.integer.appirator_significant_events_until_prompt) <= significantEventCount) {
+                    showRateDialog(mContext, editor);
+                    return;
+                }
+            }
+        }
     }
 }
